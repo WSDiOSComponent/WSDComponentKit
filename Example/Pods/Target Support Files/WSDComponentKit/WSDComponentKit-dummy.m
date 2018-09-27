76ff07fd45e3eb4afe9ce216ee5486b97eac59a4
wangsidi@baichebao.com <wangsidi@baichebao.com>
2018-09-27 15:54:43 +0800

    test

---
+++ Example/Pods/Target Support Files/WSDComponentKit/WSDComponentKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSDComponentKit : NSObject
+@end
+@implementation PodsDummy_WSDComponentKit
+@end
---
+++ Example/Pods/Target Support Files/WSDComponentKit/WSDComponentKit-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Decorator.h"
+#import "Entrypt.h"
+#import "NetWork.h"
+#import "Net_Decorator.h"
+#import "Net_Example.h"
+#import "Person.h"
+#import "Shoes.h"
+#import "Tshit.h"
+
+FOUNDATION_EXPORT double WSDComponentKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char WSDComponentKitVersionString[];
+
